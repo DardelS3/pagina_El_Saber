d7203a708c31effe995d0e26e280715ddbcf581f
steve <stevebeteta@gmail.com>
2018-10-21 20:14:43 -0600

    [0.2.3] Header Listo, ahora a componer el resto de la página

--- js/index.js
+++ js/index.js
@@ -1,9 +1,1 @@
 $('.carousel').carousel()
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
